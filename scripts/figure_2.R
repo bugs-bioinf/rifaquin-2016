a68565d7644aed45f427c78a3335ce90f9f82215
Adam Witney <awitney@sgul.ac.uk>
2017-03-29 11:18:57 +0100

    Fixing data download scripts

--- scripts/figure_2.R
+++ scripts/figure_2.R
@@ -5,7 +5,7 @@ library(phangorn)
 tru <- read.tree("phylogeny/RAxML_bipartitions.NC_000962.b1")
 trr <- midpoint(tru)
 
-plotTree(trr, infoFile="scripts/tipinfo.csv", colourNodesBy ="class", tip.colour.cex=3, lwd=2, tipColours = c("blue","red", "green", "black"), tip.labels=T, tipLabelSize=2.5, offset=0.003, legend=F, infoCols=NA, outputPNG="figures/figure_2.png",w=1000,h=1500,closeDev=F)
+plotTree(trr, infoFile="scripts/tipinfo.csv", colourNodesBy ="class", tip.colour.cex=3, lwd=2, tipColours = c("blue","red", "green", "black"), tip.labels=T, tipLabelSize=2.5, offset=0.003, legend=F, infoCols=NA, outputPNG="figures/figure_2.png",w=8000,h=12000,res=576,closeDev=F)
 edgelabels("Euro American", edge=c(137), adj = c(0.5, -0.5), frame="none",cex=2.5)
 edgelabels("East Asian", edge=c(138), adj = c(0.5, -0.5), frame="none",cex=2.5)
 edgelabels("East African Indian", edge=c(145), adj = c(0.5, -0.5), frame="none",cex=2.5)
